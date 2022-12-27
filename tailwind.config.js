09979865b6d71d62040982f34093d3fa8f2f3d67
Andrei Maxim <andrei@andreimaxim.ro>
2022-12-27 08:35:31 +0200

    Add Tailwind and Node config files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_drafts/**/*.md',
+    './_includes/**/*.liquid',
+    './_layouts/**/*.liquid',
+    './_posts/**/*.md',
+    './*.md'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
