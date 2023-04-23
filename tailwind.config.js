c234593a1a099f805ffbec99887838a7f2335907
Andrei Maxim <andrei@andreimaxim.ro>
2023-04-23 23:54:22 +0300

    Improve typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,32 @@ module.exports = {
         './*.md'
     ],
     theme: {
-        extend: {
-            typography: {
-                DEFAULT: {
-                    css: {
-                        maxWidth: "80ch"
-                    }
-                }
-            }
+        fontFamily: {
+            sans: [
+                'Inter',
+                'ui-sans-serif',
+                'system-ui',
+                '-apple-system',
+                'BlinkMacSystemFont',
+                'Segoe UI',
+                'Roboto',
+                'Helvetica Neue',
+                'Arial',
+                'Noto Sans',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol',
+                'Noto Color Emoji'
+            ],
+            bold: [
+                'Inter'
+            ],
+            italic: [
+                'Inter'
+            ]
         },
+        extend: {},
     },
     plugins: [
         require('@tailwindcss/typography')
