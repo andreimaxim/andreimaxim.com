5900e942319e9cd396419b72b232048547296ea6
Andrei Maxim <andrei@andreimaxim.ro>
2024-04-03 18:21:08 +0300

    Use the default system-ui typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './_drafts/**/*.md',
-        './_includes/**/*.liquid',
-        './_layouts/**/*.liquid',
-        './_posts/**/*.md',
-        './*.html'
+  content: [
+    './_drafts/**/*.md',
+    './_includes/**/*.liquid',
+    './_layouts/**/*.liquid',
+    './_posts/**/*.md',
+    './*.html'
     ],
-    theme: {
-        fontFamily: {
-            sans: [
-                'Inter',
-                'ui-sans-serif',
-                'system-ui',
-                '-apple-system',
-                'BlinkMacSystemFont',
-                'Segoe UI',
-                'Roboto',
-                'Helvetica Neue',
-                'Arial',
-                'Noto Sans',
-                'sans-serif',
-                'Apple Color Emoji',
-                'Segoe UI Emoji',
-                'Segoe UI Symbol',
-                'Noto Color Emoji'
-            ],
-            bold: [
-                'Inter'
-            ],
-            italic: [
-                'Inter'
-            ]
-        },
-        extend: {},
-    },
-    plugins: [
-        require('@tailwindcss/typography')
-    ],
+  plugins: [
+    require('@tailwindcss/typography')
+  ]
 }
