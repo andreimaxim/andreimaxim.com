c66086f4fe4a9c004022cb78d7c43020c049f44e
Andrei Maxim <andrei@andreimaxim.ro>
2024-04-13 20:49:32 +0300

    Slight refactoring for the fingerprinting module

--- _plugins/drive_shaft/assembly.rb
+++ _plugins/drive_shaft/assembly.rb
@@ -20,7 +20,8 @@ module Jekyll
           assets[url]
         end
 
-        def fingerprint_assets!
+        def create_fingerprinted_assets!
+          # [TODO]: Replace the asset paths within the assets to use the fingerprinted versions
           assets.values.each { |asset| asset.create_fingerprinted_copy! }
         end
       end
@@ -30,20 +31,20 @@ end
 
 Jekyll::Hooks.register(:site, :pre_render) do |site|
   site.static_files_to_write.each do |file|
-    Jekyll::DriveShaft::Assembly.add(
-      url: file.url,
-      source_path: file.path,
-      destination_path: file.destination(site.dest)
-    )
+    url = file.url
+    source_path = file.path
+    destination_path = file.destination(site.dest)
+
+    Jekyll::DriveShaft::Assembly.add(url: url, source_path: source_path, destination_path: destination_path)
   end
 end
 
-Jekyll::Hooks.register([:documents, :posts, :pages], :post_render) do |document|
+Jekyll::Hooks.register(:documents, :post_render) do |document|
   Jekyll::DriveShaft::Assembly.assets.each do |url, asset|
     document.output.gsub!(url, asset.digested_url)
   end
 end
 
 Jekyll::Hooks.register(:site, :post_write) do
-  Jekyll::DriveShaft::Assembly.fingerprint_assets!
+  Jekyll::DriveShaft::Assembly.create_fingerprinted_assets!
 end
