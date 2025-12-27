2f2b7fb8e8ba7b8c4aa147c17ec8b76ba5167c0c
Andrei Maxim <andrei@andreimaxim.ro>
2025-12-27 15:06:34 +0200

    Use server-side rendering for syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,20 @@
-module.exports = function(eleventyConfig) {
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
+
   // Copy assets to output
   eleventyConfig.addPassthroughCopy("assets");
-  
+
   // Copy specific files
   eleventyConfig.addPassthroughCopy("feed.xml");
   eleventyConfig.addPassthroughCopy("sitemap.xml");
-  
+
   // Set up collections
-  eleventyConfig.addCollection("posts", function(collectionApi) {
+  eleventyConfig.addCollection("posts", function (collectionApi) {
     return collectionApi.getFilteredByGlob("posts/*.md").reverse();
   });
-  
+
   // Parse dates from filenames for Jekyll-style posts
   eleventyConfig.addGlobalData("eleventyComputed", {
     date: function (data) {
@@ -24,9 +28,9 @@ module.exports = function(eleventyConfig) {
       return data.date;
     }
   });
-  
+
   // Transform permalinks for posts
-  eleventyConfig.addFilter("postPermalink", function(inputPath) {
+  eleventyConfig.addFilter("postPermalink", function (inputPath) {
     if (inputPath.includes('posts/')) {
       const filename = inputPath.split('/').pop();
       const slug = filename.replace(/^\d{4}-\d{2}-\d{2}-/, '').replace(/\.md$/, '');
@@ -34,24 +38,24 @@ module.exports = function(eleventyConfig) {
     }
     return inputPath;
   });
-  
+
   // Date filter
-  eleventyConfig.addFilter("date", function(date, format) {
+  eleventyConfig.addFilter("date", function (date, format) {
     const d = new Date(date);
     const months = ['January', 'February', 'March', 'April', 'May', 'June',
-                   'July', 'August', 'September', 'October', 'November', 'December'];
-    
+      'July', 'August', 'September', 'October', 'November', 'December'];
+
     if (format === "%B %e, %Y") {
       return `${months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
     }
     return d.toISOString().split('T')[0];
   });
-  
+
   // Add base URL for canonical and social meta tags
-  eleventyConfig.addGlobalData("baseUrl", function() {
+  eleventyConfig.addGlobalData("baseUrl", function () {
     return process.env.NODE_ENV === "production" ? "https://andreimaxim.com" : "http://localhost:8080";
   });
-  
+
   return {
     dir: {
       input: ".",
